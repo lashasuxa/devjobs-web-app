f7812133bf84a5b0d0146614d02a7bc4c59f675b
lashasuxa <lashasukhashvili1995@gmail.com>
2023-06-27 23:30:17 -0400

    fix:file structure

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import {
 } from "./controllers/project-controller.js";
 import bodyParser from "body-parser";
 import cors from "cors";
+import swaggerMiddleware from "./middlewares/swagger-middleware.js";
 
 dotenv.config();
 connect();
@@ -15,11 +16,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "app works fine" });
-});
+// app.get("/", (req, res) => {
+//   return res.status(200).json({ message: "app works fine" });
+// });
 
 app.get("/api/projects", getAllProject);
 app.get("/api/projects/web", getWebProject);
+app.use("/", ...swaggerMiddleware());
 
 app.listen(3000);
